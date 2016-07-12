6ae4e859fc453b2d992b280c5e8c1491880f6715
Raj Tomar <rajtomar50@gmail.com>
2016-07-12 18:57:38 +0530

    daoimpl implemented

--- SpringWebServiceExample/src/com/raj/daoImpl/EmployeeDaoImpl.java
+++ SpringWebServiceExample/src/com/raj/daoImpl/EmployeeDaoImpl.java
@@ -1,7 +1,13 @@
 package com.raj.daoImpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.raj.beans.EmployeeBean;
@@ -9,15 +15,54 @@ import com.raj.dao.EmployeeDao;
 
 @Repository
 public class EmployeeDaoImpl implements EmployeeDao{
+	
+	@Autowired
+	SessionFactory sessionFactory;
+	private Session session = null;
+	private Transaction tx = null;
 
 	@Override
 	public String saveOrUpdateEmployee(EmployeeBean bean) {
+		try{
+			session = sessionFactory.getCurrentSession();
+			tx = session.beginTransaction();
+			if(null == bean.getId()){
+				session.save(bean);
+			}else{
+				session.update(bean);
+			}
+			tx.commit();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		finally{
+			if(session.isOpen()){
+				session.close();
+			}
+		}
 		return null;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<EmployeeBean> getEmployeeList() {
-		return null;
+		List<EmployeeBean> list = new ArrayList<EmployeeBean>();
+		try{
+			session = sessionFactory.getCurrentSession();
+			tx = session.beginTransaction();
+			Query<EmployeeBean> query = session.createQuery("From EmployeeBean");
+			list = query.getResultList();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		finally{
+			if(session.isOpen()){
+				session.close();
+			}
+		}
+		return list;
 	}
 
 	@Override
